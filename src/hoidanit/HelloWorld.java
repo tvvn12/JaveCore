e47f172a0037a696ba1bef7d3d02093b598c0ccd
ttvn12 <tvvn12@gmail.com>
2024-03-16 20:01:10 +0700

    init project

---
+++ src/hoidanit/HelloWorld.java
@@ -0,0 +1,11 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+
+}
